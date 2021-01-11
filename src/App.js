4017e12da80c9a2e4fd6808cfc207558f12f7ee3
Arjun Kothakota <arjunkothakota@Arjuns-MacBook-Pro.local>
2021-01-11 17:15:12 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import './App.css';
 import CallWar from './components/CallWar'
 import Word from './components/Word'
-import { Container, Row, Col, Navbar, NavbarBrand, Nav } from 'react-bootstrap'
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap'
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
 
 
 
 function App() {
   return (
     <div>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Container className="p-0" fluid={true}>
           <Navbar className="border-bottom" bg="white" expand="lg">
             <Navbar.Brand>Bets</Navbar.Brand>
             <Navbar.Toggle aria-controls="navbar-toggle" />
             <Navbar.Collapse id="navbar-toggle">
               <Nav className="ml-auto">
-                <Link className="nav-link" to="/Bets/">Call War</Link>
-                <Link className="nav-link" to="/Bets/the-word/">The Word</Link>
+                <Link className="nav-link" to="/">Call War</Link>
+                <Link className="nav-link" to="/the-word">The Word</Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
 
-          <Route path="/Bets/" exact render={() => <CallWar />} />
-          <Route path="/Bets/the-word/" exact render={() => <Word />} />
+          <Route path="/" exact render={() => <CallWar />} />
+          <Route path="/the-word" exact render={() => <Word />} />
         </Container>
       </Router>
       
